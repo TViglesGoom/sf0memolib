3d0811a720ff282d26460d7ed11b91749a100881
TViglesGoom <tviglesgoom@gmail.com>
2021-01-11 23:34:26 +0200

    search input redesign

--- web-ui/components/common/header.vue
+++ web-ui/components/common/header.vue
@@ -1,17 +1,19 @@
 <template>
   <header>
     <div id="web-ui-header">
-      <div id="right-part">
+      <div id="left-part">
         <nav>
           <a id="memolib-link">MemoLib ( {{ numOfMemosInCollection }} docs)</a>
         </nav>
-
+      </div>
+      <div id="right-part">
         <form @submit.prevent="triggerSearch">
           <div>
             <span>
               <button @click="triggerSearch"></button>
             </span>
             <input
+              id="search"
               v-model="searchTerm"
               type="search"
               placeholder="keyword search memo library"
@@ -36,103 +38,27 @@
   #left-part {
     flex: 1;
     display: inline-flex;
-    text-align: center;
-    align-items: center;
     justify-content: center;
-    #logo {
-      text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-      @keyframes flashing {
-        0% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        3% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        10% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        10.3% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        12% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        14% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        14.3% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        16% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        18% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        18.3% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        20% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        22% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        36% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        36.3% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        38% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        40% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        76% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        76.3% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        78% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        80% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        80.3% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        82% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        84% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        84.3% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 0.3);
-        }
-        86% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-        100% {
-          text-shadow: 0 0 0.9em rgba(255, 255, 255, 1);
-        }
-      }
-
-      animation: flashing 6s infinite;
-    }
+    align-items: center;
   }
 
   #right-part {
     display: inline-flex;
     flex: 1;
-    justify-content: space-evenly;
+    justify-content: center;
     align-items: center;
     #memolib-link {
       margin-right: 20px;
+    }
+    #search {
+      background-color: transparent;
+      border: solid 1px #fff;
+      width: 400px;
+      &:focus {
+        -webkit-box-shadow: none;
+        box-shadow: inset 0 0 0.8em rgba(255, 255, 255, 0.3),
+          0 0 0.8em rgba(255, 255, 255, 0.3);
+      }
     }
   }
 }
