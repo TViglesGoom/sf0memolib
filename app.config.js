04ba1990374c9a35ce09dde17c20231344aeb022
TViglesGoom <tviglesgoom@gmail.com>
2021-01-11 22:32:37 +0200

    moved db collection name from .env to app.config

---
+++ app.config.js
@@ -0,0 +1,3 @@
+export default {
+  defaultCollection: "ported-memos",
+}
--- web-api/memo.lib.js
+++ web-api/memo.lib.js
@@ -4,8 +4,9 @@ const IGNORE_GLOBS = ['.obsidian', 'attachments', '*.css'];
 import dotenv from 'dotenv'
 dotenv.config({ path: '../.env' })
 
-const { COUCHDB_HOST, COUCHDB_PORT, COUCHDB_USER, COUCHDB_PWD, COUCHDB_MEMOS_COLLECTION } = process.env;
-const COUCHDB_DATABASE_URL = `http://${COUCHDB_USER}:${COUCHDB_PWD}@${COUCHDB_HOST}:${COUCHDB_PORT}/${COUCHDB_MEMOS_COLLECTION}`
+const { defaultCollection } = require('../app.config')
+const { COUCHDB_HOST, COUCHDB_PORT } = process.env
+const COUCHDB_DATABASE_URL = `http://${COUCHDB_HOST}:${COUCHDB_PORT}/${defaultCollection}`
 
 import util from 'util';
 import { exec } from 'child_process';
